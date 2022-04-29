0ef2e4633793233cf9fa898b62c17f693c8e95dd
Dmitrii-Shatalin <62507996+Dmitrii-Shatalin@users.noreply.github.com>
2022-04-29 21:51:13 +0300

    fix

--- modules/task_1/shatalin_d_sort_odd_even_merge_double/sort.cpp
+++ modules/task_1/shatalin_d_sort_odd_even_merge_double/sort.cpp
@@ -15,8 +15,7 @@ std::vector<double> countingSort(std::vector<double> array, int numByte) {
     if (numByte != 7) {
         for (int i = 1; i < 256; i++)
             offset[i] = offset[i - 1] + counter[i - 1];
-    }
-    else {
+    } else {
         offset[255] = counter[255] - 1;
         for (int i = 254; i >= 128; i--)
             offset[i] = offset[i + 1] + counter[i];
@@ -27,13 +26,11 @@ std::vector<double> countingSort(std::vector<double> array, int numByte) {
     if (numByte != 7) {
         for (size_t i = 0; i < array.size(); i++)
             ans[offset[byteArray[8 * i + numByte]]++] = array[i];
-    }
-    else {
+    } else {
         for (size_t i = 0; i < array.size(); i++)
             if (byteArray[8 * i + numByte] >= 128) {
                 ans[offset[byteArray[8 * i + numByte]]--] = array[i];
-            }
-            else {
+            } else {
                 ans[offset[byteArray[8 * i + numByte]]++] = array[i];
             }
     }
@@ -51,8 +48,7 @@ std::vector<double> getRandomVector(int size, double lower_bound, double upper_b
     std::mt19937 mt;
     if (seed == -1) {
         mt = std::mt19937(static_cast<int>(time(0)));
-    }
-    else {
+    } else {
         mt = std::mt19937(seed);
     }
     std::uniform_real_distribution<double> rand(-500.0, 500.0);
